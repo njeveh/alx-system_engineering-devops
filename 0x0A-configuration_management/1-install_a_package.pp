4da0decfcc25fae590edef79d49f37d44f119b68
njeveh <njeveh@gmail.com>
2024-06-29 00:37:34 +0300

    update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,21 @@
-# Puppet code to install a package puppet-lint
+# Using Puppet to install Flask v2.1.0 from pip3
 
-package { 'puppet-lint':
+package { 'Flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3',
+  require  => [
+    File['pip3'],
+    Package['Werkzeug']
+  ]
+}
+file { 'pip3':
+  ensure => 'present',
+  path   => '/usr/bin/pip3'
+}
+package { 'Werkzeug':
   ensure   => '2.1.1',
-  provider => 'gem'
+  name     => 'Werkzeug',
+  provider => 'pip3',
+  require  => File['pip3']
 }

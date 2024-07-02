8df38c5ae178e08d0bebed1cf3a6fd67378ac263
njeveh <njeveh@gmail.com>
2024-07-02 17:03:16 +0300

    add updates

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 # connect with puppet
 file_line { 'Declare_identity_file':
   path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/holberton',
+  line    => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn_off_passwd_auth':

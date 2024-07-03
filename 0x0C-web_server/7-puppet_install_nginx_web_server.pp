d908bfe0074780e0496f75ab2e71d34ce386a754
njeveh <njeveh@gmail.com>
2024-07-03 04:19:57 +0300

    add updates

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,7 +9,7 @@ file_line { 'server_config':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/ permanent;',
 }
 
 file { '/var/www/html/index.html':
